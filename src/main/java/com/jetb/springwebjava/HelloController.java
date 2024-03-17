09714e7d31d05f5d7f3ccbb5922fdcb4f661dc43
Dmitry Beznosiuk <145875719+DmitryBizCode@users.noreply.github.com>
2024-03-18 00:50:32 +0200

    simple hello using html

--- src/main/java/com/jetb/springwebjava/HelloController.java
+++ src/main/java/com/jetb/springwebjava/HelloController.java
@@ -1,12 +1,18 @@
 package com.jetb.springwebjava;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
-@RestController
+@Controller
 public class HelloController {
-    @GetMapping("/hello")
-    public String SayHello(){
-        return "Hello World Dima";
+    @GetMapping("/")
+    public String getMainPage(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        String newName = "Hello " + name;
+        model.addAttribute("newName", newName); // Use "newName" instead of "name"
+        return "mainD"; // Return the template name without the file extension
     }
 }
+
+
--- src/main/java/com/jetb/springwebjava/SpringWebJavaApplication.java
+++ src/main/java/com/jetb/springwebjava/SpringWebJavaApplication.java
@@ -2,12 +2,11 @@ package com.jetb.springwebjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class SpringWebJavaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebJavaApplication.class, args);
 	}
-
 }
