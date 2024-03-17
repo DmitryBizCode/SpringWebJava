4de1e36072194fba857f0dc5486733c5af0305ae
Dmitry Beznosiuk <145875719+DmitryBizCode@users.noreply.github.com>
2024-03-17 13:44:17 +0200

    Initial commit

---
+++ src/main/java/com/jetb/springwebjava/HelloController.java
@@ -0,0 +1,12 @@
+package com.jetb.springwebjava;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String SayHello(){
+        return "Hello World Dima";
+    }
+}
